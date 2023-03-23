96d83b0fd4d0884d23745dd6f1ece081f5643bc1
Isaac M Bazian <bazia001@umn.edu>
2023-03-23 14:33:57 -0500

    Data import and cleaning complete

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -1,12 +1,22 @@
-# Script Settings and Resources
+## Script Settings and Resources
 library(tidyverse)
 library(jsonlite)
 
 
 
+## Data Import and Cleaning
+# Reads in the data as a list from Reddit via the JSON API and flattens it so that pulling out what's needed is easier
+rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json", flatten = TRUE)
+# Gets the per-post data out of the list
+rstats_original_tbl <- as_tibble(rstats_list$data$children)
+
+# Takes original tbl with all variables, duplicates variables of interest with new names, and keeps only those newly named variables 
+rstats_tbl <- rstats_original_tbl %>% 
+  mutate(post = data.title,
+         upvotes = data.score,
+         comments = data.num_comments) %>% 
+  select(post, upvotes, comments)
 
 
-# Data Import and Cleaning
-rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json")
 
-rstats_original_tbl <- rstats_list[["data"]]["children"]
+
