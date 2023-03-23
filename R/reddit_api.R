dad09cfc5ab9e9b6be018e6869d1536c7705cb4c
Isaac M Bazian <bazia001@umn.edu>
2023-03-23 09:14:43 -0500

    Read in JSON as list, stuck on next step. Stopping for now, will try the webscraping one.

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -1,2 +1,12 @@
 # Script Settings and Resources
 library(tidyverse)
+library(jsonlite)
+
+
+
+
+
+# Data Import and Cleaning
+rstats_list <- fromJSON("https://www.reddit.com/r/rstats/.json")
+
+rstats_original_tbl <- rstats_list[["data"]]["children"]
