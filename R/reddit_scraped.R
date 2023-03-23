86c3d036e717a2032a3baa3cd182fd8d55c9b0e2
Isaac M Bazian <bazia001@umn.edu>
2023-03-23 09:39:59 -0500

    Successfully scraped post text

--- R/reddit_scraped.R
+++ R/reddit_scraped.R
@@ -5,3 +5,8 @@ library(rvest)
 
 # Data Import and Cleaning
 rstats_html <- read_html("https://old.reddit.com/r/rstats/")
+
+post <- rstats_html %>% 
+  html_elements(xpath = "//p[@class = 'title']") %>% 
+  html_text()
+  
